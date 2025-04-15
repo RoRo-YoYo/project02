9afcd045154744a0ebbdeee928dd0a0fb4997c41
RoRo-YoYo <RocelynYoung2027@u.northwestern.edu>
2025-04-15 16:45:38 -0500

    Step 1 completed, set up code and output number of nodes

---
+++ main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+
+#include "building.h"
+#include "node.h"
+#include "nodes.h"
+#include "osm.h"
+#include "tinyxml2.h"
+
+
+using namespace std;
+using namespace tinyxml2;
+
+
+int main() {
+
+XMLDocument xmldoc;
+
+cout << "** NU open street map **" << endl;
+
+string filename;
+
+cout << endl;
+cout << "Enter map filename>" << endl;
+
+
+getline(cin, filename);
+
+if (!osmLoadMapFile(filename, xmldoc)) {
+    return 0;}
+
+    Nodes object(xmldoc);
+    int num_of_nodes = object.getNumOsmNodes();
+    
+    cout << "# of nodes: " << num_of_nodes << endl;    
+}
